f4f6cd464921e7114f54521e4c84db1ca5b357d7
zhenya <fivenne@gmail.com>
2024-07-16 21:50:57 +0300

    restart

--- Assets/Scripts/RestartButton.cs
+++ Assets/Scripts/RestartButton.cs
@@ -1,9 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class RestartButton : MonoBehaviour
+public class RestartButton : EventTrigger
 {
     public void OnClick()
     {
